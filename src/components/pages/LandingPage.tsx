9c846ec64a6afa52e5abc97fd5b9f2fa2e538de3
JEstes93 <jestes93jobs@gmail.com>
2020-10-27 20:34:07 -0500

    Centered login panel, and removed landing page heading

--- src/components/pages/LandingPage.tsx
+++ src/components/pages/LandingPage.tsx
@@ -1,30 +1,13 @@
 import React from 'react';
-import {} from 'recoil';
 import Login from '../Login';
-import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
-const useStyles = makeStyles((theme) => ({
-  heading: {
-    padding: theme.spacing(10),
-  },
-  paper: {
-    height: 300,
-    width: 300,
-    justify: 'center',
-  },
-  control: {
-    padding: theme.spacing(15),
-  },
-}));
+import { Grid } from '@material-ui/core';
 
 export default function LandingPage() {
-  const classes = useStyles();
   return (
-    <>
-      <Typography className={classes.heading} variant="h1">
-        LandingPage
-      </Typography>
-      <Login />
-    </>
+    <Grid container justify="center" alignItems="center" direction="column" style={{ height: '90vh' }}>
+      <Grid container>
+        <Login />
+      </Grid>
+    </Grid>
   );
 }
