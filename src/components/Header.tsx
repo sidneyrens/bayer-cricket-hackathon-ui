224277666134088eaace462633e5da91c15e879f
Sidney Rens <sidney.rens.ext@bayer.com>
2020-10-28 08:39:09 -0500

    added an api endpoint

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,6 +6,7 @@ import { gameStatus, settingsVisible } from '../recoil/atoms';
 import { AccountCircle, ExitToAppOutlined, Settings } from '@material-ui/icons';
 import { selectGameState } from '../recoil/selectors';
 import { GameStatus } from '../util/types';
+import { fetchQuestions } from '../data/fetchQuestions'
 
 export default function Header(): JSX.Element {
   const setShowSettings = useSetRecoilState(settingsVisible);
@@ -13,7 +14,9 @@ export default function Header(): JSX.Element {
   const history = useHistory();
   const location = useLocation();
   const resetGameState = useResetRecoilState(selectGameState);
+  const questions = fetchQuestions();
 
+  console.log('Questions', questions)
   return (
     <Grid container direction="row" className="navHeader" xs={12} justify="space-between">
       <Grid container direction="row" xs={4} justify="flex-start" alignItems="center">
---
+++ src/data/fetchQuestions.js
@@ -0,0 +1,14 @@
+export const fetchQuestions = async () => {
+    try {
+      await fetch(
+        "https://47ergmkdli.execute-api.us-east-1.amazonaws.com/fetchQuestions"
+      )
+        .then(response => response.json())
+        .then(data => {
+          console.log("Success:", data);
+          return data
+        });
+    } catch (err) {
+      console.log(err);
+    }
+  };
